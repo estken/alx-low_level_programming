4833e92a53cb0b8d14995dad10c0f564844f5c2c
root <kelechiobidike@gmail.com>
2022-03-21 12:26:05 -0700

    Create 0x05 directory and file 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98: takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @*n: parameter
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: integer paremeter that be pointed
  * void: nothing to return
  */
 void reset_to_98(int *n)
